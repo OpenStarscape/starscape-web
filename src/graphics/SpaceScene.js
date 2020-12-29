b5858157136422b49a9d2bc16e9aa778b25f11d9
William Wold <wm@wmww.sh>
2020-12-29 13:22:39 -0800

    Clean up SpaceScene

--- src/graphics/SpaceScene.js
+++ src/graphics/SpaceScene.js
@@ -7,34 +7,23 @@ import { makeBody } from "../graphics/Body.js";
 /// Manages everything required to render a 3D space view with three.js.
 export default class SpaceScene {
   constructor(connection) {
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    const renderer = new THREE.WebGLRenderer({antialias: true});
-
     this.lt = new Lifetime();
-    this.scene = scene;
-    this.camera = camera;
-    this.renderer = renderer;
+    this.connection = connection;
+    this.scene = new THREE.Scene();
 
-    camera.position.z = 100;
+    this.renderer = new THREE.WebGLRenderer({antialias: true});
+    this.renderer.setClearColor('black');
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
 
-    renderer.setClearColor("#000000");
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    this.cameraController = new TrackballControls(this.camera, this.renderer.domElement);
 
-    this.addControllers();
     this.starfield = new Starfield(this.lt, this.scene);
 
-    this.connection = connection;
     this.connection.god.property('bodies').getThen(this.lt, bodies => {
       bodies.forEach(obj => {
         makeBody(this.lt, obj, body => {
           this.scene.add(body.mesh);
-          //if (!this.currentShip && body.isShip()) {
-            //body.mesh.attach(this.camera);
-            //this.currentShip = body;
-            //this.camera.position.set(0.0, 0.0, 20.0);
-            //this.camera.rotation.set(0.0, 0.0, 0.0);
-          //}
         });
       });
       this.connection.god.action('create_ship').fire([
@@ -55,9 +44,9 @@ export default class SpaceScene {
     });
 
     document.addEventListener("resize", () => {
-      renderer.setSize(window.innerWidth, window.innerHeight);
-      camera.aspect = window.innerWidth / window.innerHeight;
-      camera.updateProjectionMatrix();
+      this.renderer.setSize(window.innerWidth, window.innerHeight);
+      this.camera.aspect = window.innerWidth / window.innerHeight;
+      this.camera.updateProjectionMatrix();
     });
 
     this.render();
@@ -65,26 +54,6 @@ export default class SpaceScene {
 
   domElement() {
     return this.renderer.domElement;
-  }
-
-  createShip() {
-    const ship = new THREE.ConeGeometry(2, 10, 50, 10);
-    const material = new THREE.MeshStandardMaterial({color: 0xff00ff, side: THREE.DoubleSide});
-    const mesh = new THREE.Mesh(ship, material);
-
-    this.ship = mesh;
-    this.scene.add(mesh);
-  }
-
-  addControllers() {
-    console.log("ADDED CONTROLLER");
-    this.cameraController = new TrackballControls(this.camera, this.renderer.domElement);
-  }
-
-  move(obj, direction) {
-    obj.position.x += direction.x;
-    obj.position.y += direction.y;
-    obj.position.z += direction.z;
   }
 
   render() {
