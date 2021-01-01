1e96c6f46ac31b3cf29e1a0216a7c317ad12c162
William Wold <wm@wmww.sh>
2020-12-31 23:22:31 -0800

    get ListProperty on the lifetime system

--- src/lib/ListProperty.js
+++ src/lib/ListProperty.js
@@ -1,14 +1,16 @@
 import {ActionElement} from '../lib/Element.js';
+import Lifetime from "../lib/Lifetime.js";
 
 export default class ListProperty {
   constructor(property, lifetime) {
+    this.lt = new Lifetime();
+    lifetime.add(this.lt);
+    property.lifetime().add(this.lt);
+    this.lt.add(this);
     this.items = new Map();
     this.itemAdded = new ActionElement();
-    this.itemDeleted = new ActionElement();
-    lifetime.add(this.itemAdded);
-    lifetime.add(this.itemDeleted);
-    lifetime.add(this);
-    property.subscribe(lifetime, list => {
+    this.lt.add(this.itemAdded);
+    property.subscribe(this.lt, list => {
       if (!Array.isArray(list)) {
         throw 'ListProperty given value ' + list + ' which is not array';
       }
@@ -18,6 +20,9 @@ export default class ListProperty {
         const item = list[i];
         const value = oldItems.get(item);
         if (value === undefined) {
+          if (this.items.has(item)) {
+            throw 'ListProperty has duplicate item ' + item + '. All items must be unique';
+          }
           this.items.set(item, null);
           this.itemAdded.fire(item);
         } else {
@@ -25,26 +30,34 @@ export default class ListProperty {
         }
         oldItems.delete(item);
       }
-      for (const pair of oldItems) {
-        this.itemDeleted.fire(pair);
+      for (const [/*key*/, val] of oldItems) {
+        if (val) {
+          this.lt.disposeOf(val);
+        }
       }
     });
   }
 
+  /// Sets the given key to the given value. Key must already be known. Value can either be an
+  /// object with a .dispose() or null. If there is already a value, it is replaced and it is
+  /// disposed. Replacing a value with itself is not allowed (because it would be disposed in the
+  /// process and should not be used after that).
   set(key, val) {
+    const old = this.items.get(key);
     this.items.set(key, val);
+    if (val) {
+      this.lt.add(val);
+    }
+    if (old) {
+      this.lt.disposeOf(old);
+    }
   }
 
   get(key) {
     return this.items.get(key);
   }
 
-  // TODO: handle destruction of the property
-
   dispose() {
-    for (const pair of this.items) {
-      this.itemDeleted.fire(pair);
-    }
     this.items.clear();
   }
 }
