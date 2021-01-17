345787b379279e1f99bd66d3abcec20014fd17cd
Sophie Winter <git@phie.me>
2021-01-17 00:07:53 -0800

    Clean up StarscapeSet

--- src/lib/StarscapeSet.js
+++ src/lib/StarscapeSet.js
@@ -1,19 +1,19 @@
 import {StarscapeObject} from '../lib/Starscape.js';
-import Lifetime from "../lib/Lifetime.js";
 
 /// Keeps track of a starscape property that is a set (a list of items that are guaranteed to be
 /// unique and are in an arbitrary order). The callback is given two arguments whenever a new item
 /// is added to the set: the lifetime for which the item is in the set and the item.
 export default class StarscapeSet {
   constructor(property, lifetime, callback) {
-    this.lt = new Lifetime();
-    lifetime.add(this.lt);
-    property.lifetime().add(this.lt);
+    this.lt = lifetime.newChild()
+    property.lifetime().addChild(this.lt);
     this.lt.add(this);
     this.items = new Map();
+    this.callbacks = []; // lists containing [lifetime, callback]
     property.subscribe(this.lt, list => {
       if (!Array.isArray(list)) {
-        throw 'ListProperty given value ' + list + ' which is not array';
+        console.error('StarscapeSet given value ' + list + ' which is not array');
+        return;
       }
       const oldItems = this.items;
       this.items = new Map();
@@ -21,24 +21,28 @@ export default class StarscapeSet {
         const item = list[i];
         const oldItemLifetime = oldItems.get(item);
         if (oldItemLifetime === undefined) {
+          // If this item was not previously in the set
           if (this.items.has(item)) {
-            throw 'ListProperty has duplicate item ' + item + '. All items must be unique';
+            console.error(
+              'StarscapeSet has duplicate item ' +
+              item +
+              '. All items should be unique');
+              continue;
           }
-          const itemLifetime = new Lifetime();
-          this.lt.add(itemLifetime);
+          const itemLifetime = this.lt.newChild();
           // if it's an object, should die with object (though the server should remove it for us)
           if (item instanceof StarscapeObject) {
-            item.lifetime().add(itemLifetime);
+            item.lifetime().addChild(itemLifetime);
           }
           this.items.set(item, itemLifetime);
           callback(itemLifetime, item);
         } else {
+          oldItems.delete(item);
           this.items.set(item, oldItemLifetime);
         }
-        oldItems.delete(item);
       }
       for (const [/*item*/, oldItemLifetime] of oldItems) {
-        this.lt.disposeOf(oldItemLifetime);
+        oldItemLifetime.dispose()
       }
     });
   }
