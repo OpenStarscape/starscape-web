6750ef3bd6aa1ba5c9c090915844418e23b00580
Sophie Winter <git@phie.me>
2021-02-07 19:47:33 -0800

    Port ShipNameField to a react component

---
+++ src/components/ShipNameField.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+export default class ShipNameField extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {name: '', promptForName: true};
+    // TODO: subscribe to ship name
+  }
+
+  handleInput(evt) {
+    let name = evt.target.value;
+    if (!name) {
+      name = null;
+    }
+    this.setState({name: name, promptForName: false});
+    const ship = this.props.game.currentShip.get();
+    if (ship) {
+      ship.property('name').set(name);
+    }
+  }
+
+  render() {
+    const promptStyle = {fontSize: 'x-large', color: 'yellow'};
+    return (
+      <div>
+        <input value={this.state.value} onChange={evt => this.handleInput(evt)}/>
+        {this.state.promptForName &&
+          <p style={promptStyle}>⮤ give your ship a name</p>
+        }
+      </div>
+    );
+  }
+}
