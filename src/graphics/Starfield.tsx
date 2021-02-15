2ca072a474b380a8099cddc150144190a846a464
Sophie Winter <git@phie.me>
2021-02-14 17:26:42 -0800

    Port Starfield to typescript

--- src/graphics/Starfield.tsx
+++ src/graphics/Starfield.tsx
@@ -1,20 +1,23 @@
 import * as THREE from "three";
+import Lifetime from '../lib/Lifetime';
 
 /// Adds a stary background to a 3D scene.
 export default class Starfield {
-  constructor(lifetime, scene) {
-    this.closest = 600;
-    this.farthest = 800;
-    this.lt = lifetime;
-    this.scene = scene;
-    this.geom = new THREE.Geometry();
-    this.lt.add(this.geom);
+  private closest = 600;
+  private farthest = 800;
+  private geom = new THREE.Geometry();
+  private smallMat = new THREE.PointsMaterial({ color: 0xffffff, size: 1.5 });
+  private bigMat = new THREE.PointsMaterial({ color: 0xffffff, size: 2.0 });
+  private stars: THREE.Points[] = [];
+
+  constructor(
+    readonly lt: Lifetime,
+    readonly scene: THREE.Scene
+  ) {
     this.geom.vertices.push(new THREE.Vector3());
-    this.smallMat = new THREE.PointsMaterial({ color: 0xffffff, size: 1.5 });
-    this.bigMat = new THREE.PointsMaterial({ color: 0xffffff, size: 2.0 });
-    this.lt.add(this.mat);
-    this.stars = [];
-    this.isAdded = false;
+    this.lt.add(this.geom);
+    this.lt.add(this.smallMat);
+    this.lt.add(this.bigMat);
 
     for(let i = 0; i < 100; i++) {
       this.newStar(this.bigMat);
@@ -28,7 +31,7 @@ export default class Starfield {
     }
   }
 
-  newStar(mat) {
+  newStar(mat: THREE.Material) {
     const star = new THREE.Points(this.geom, mat);
     star.position.set(
       Math.random() - 0.5,
