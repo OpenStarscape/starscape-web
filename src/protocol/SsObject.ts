5cdc9ab8b8d7e5bb5505f94540a5cd43a1c8ed02
William Wold <wm@wmww.sh>
2021-02-27 02:20:03 -0800

    Rejig SsObject member getting

--- src/protocol/SsObject.ts
+++ src/protocol/SsObject.ts
@@ -4,8 +4,9 @@ import { SsProperty } from './SsProperty'
 import { SsAction } from './SsAction'
 import { SsSignal } from './SsSignal'
 import { SsRequest } from './SsRequest'
+import { SsValue } from './SsValue'
 
-type Member<T> = new (obj: SsObject, name: string) => T
+type Member<T> = new (...args: any[]) => T
 
 /// A handle to an object on the server. Is automatically created by the connection.
 export class SsObject {
@@ -21,21 +22,33 @@ export class SsObject {
 
   /// Object must have a property with the given name. This is not automatically checked.
   property(name: string): SsProperty {
-    return this.member(name, SsProperty);
+    return this.member(
+      name,
+      SsProperty,
+      () => new SsProperty(this, name),
+    );
   }
 
   /// Object must have an action with the given name. This is not automatically checked.
   action(name: string): SsAction {
-    return this.member(name, SsAction);
+    return this.member(
+      name,
+      SsAction,
+      () => new SsAction(this, name),
+    );
   }
 
   /// Object must have an event with the given name. This is not automatically checked.
   signal(name: string): SsSignal {
-    return this.member(name, SsSignal);
+    return this.member(
+      name,
+      SsSignal,
+      () => new SsSignal(this, name),
+    );
   }
 
   /// Used internally, Get or create a property, action or event
-  private member<T>(name: string, memberClass: Member<T>): T {
+  private member<T>(name: string, memberClass: Member<T>, create: () => T): T {
     if (!this.lt.isAlive()) {
       throw new Error(
         this.id + '.' + name +
@@ -44,7 +57,7 @@ export class SsObject {
     }
     let member = this.members.get(name);
     if (!member) {
-      member = new memberClass(this, name);
+      member = create();
       this.lt.add(member);
       this.members.set(name, member);
     } else if (!(member instanceof memberClass)) {
@@ -63,21 +76,18 @@ export class SsObject {
   }
 
   /// Called by the connection.
-  handleUpdate(name: string, value: any) {
-    const member = this.member(name, SsProperty) as SsProperty;
-    member.handleUpdate(value);
+  handleUpdate(name: string, value: SsValue) {
+    this.property(name).handleUpdate(value);
   }
 
   /// Called by the connection.
   handleGetReply(name: string, value: any) {
-    const member = this.member(name, SsProperty) as SsProperty;
-    member.handleGetReply(value);
+    this.property(name).handleGetReply(value);
   }
 
   /// Called by the connection.
   handleSignal(name: string, value: any) {
-    const member = this.member(name, SsSignal) as SsSignal;
-    member.handleSignal(value);
+    this.signal(name).handleSignal(value);
   }
 
   dispose() {
