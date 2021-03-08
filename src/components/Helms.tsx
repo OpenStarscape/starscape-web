a2d0111a75190fd1cf9f583e4978676d1821cc5b
Sophie Winter <git@phie.me>
2021-03-07 21:34:58 -0800

    Rename ServerStats -> GameStats

--- src/components/Helms.tsx
+++ src/components/Helms.tsx
@@ -3,7 +3,7 @@ import { Game } from '../game';
 import { LifetimeComponent } from './LifetimeComponent';
 import SpaceScene from '../graphics/SpaceScene';
 import ShipNameField from './ShipNameField';
-import ServerStats from './ServerStats';
+import { GameStats } from './GameStats';
 import OrbitList from './OrbitList';
 
 export default class Helms extends LifetimeComponent<{game: Game}, {}> {
@@ -19,7 +19,7 @@ export default class Helms extends LifetimeComponent<{game: Game}, {}> {
         <div ref={this.divRef} style={{position: 'absolute', zIndex: 10}} />
         <div style={{position: 'absolute', zIndex: 100}}>
           <ShipNameField game={this.props.game} />
-          <ServerStats game={this.props.game} />
+          <GameStats game={this.props.game} />
           <OrbitList game={this.props.game} />
         </div>
       </div>
