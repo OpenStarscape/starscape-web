e3312c23577bb56412dbc496b52dd2684002bf9a
Sophie Winter <git@phie.me>
2021-03-07 23:46:00 -0800

    Add FPS to game stats

--- src/components/test/TestShipNameField.tsx
+++ src/components/test/TestShipNameField.tsx
@@ -8,6 +8,7 @@ test('shows prompt to enter name', () => {
     connection: null as unknown as SsConnection,
     god: null as unknown as SsObject,
     currentShip: null as unknown as unknown as LocalProperty<SsObject | null>,
+    fps: null as any,
   }
   render(<ShipNameField game={game} />);
   expect(screen.getByText('⮤ give your ship a name')).toBeInTheDocument()
--- src/game/FramerateTracker.ts
+++ src/game/FramerateTracker.ts
@@ -1,10 +1,11 @@
-import { Conduit } from '../core';
+import { Conduit, Lifetime } from '../core';
 
+/// Tracks the framerate of a single scene
 export class FramerateTracker extends Conduit<number> {
   private frames: number[] = [];
 
   constructor(
-    readonly samples: number = 10,
+    readonly samples: number = 20,
     readonly getCurrentMs: () => number = () => performance.now(),
   ) {
     super();
@@ -29,6 +30,46 @@ export class FramerateTracker extends Conduit<number> {
   }
 
   get(): number {
+    // TODO: use type system to make sure value is not undefined
+    return this.value!;
+  }
+}
+
+/// Connects to potentially multiple FramerateTrackers and reports the one with the lowest framerate
+export class FramerateReporter extends Conduit<number | null> {
+  private currentTracker: FramerateTracker | null = null;
+
+  constructor() {
+    super();
+    this.value = null;
+  }
+
+  addTracker(lt: Lifetime, tracker: FramerateTracker) {
+    tracker.subscribe(lt, fps => {
+      let newValue = this.value;
+      if (this.currentTracker === tracker) {
+        newValue = fps;
+      } else if (this.value === null || this.value === undefined || fps < this.value) {
+        this.currentTracker = tracker;
+        newValue = fps;
+      }
+      if (newValue !== this.value) {
+        this.value = newValue;
+        this.sendUpdates(newValue!);
+      }
+    });
+    lt.addCallback(() => {
+      if (this.currentTracker === tracker) {
+        this.currentTracker = null;
+        if (this.value !== null) {
+          this.value = null;
+          this.sendUpdates(null);
+        }
+      }
+    });
+  }
+
+  get(): number | null {
     // TODO: use type system to make sure value is not undefined
     return this.value!;
   }
