055a47d6b2d5fc051c8d6dd2a003e1bdbe558b56
Sophie Winter <git@phie.me>
2021-06-28 08:39:35 -0700

    Move orbit test data to an external file generated by a python script

--- src/graphics/test/TestOrbitBodySpatial.ts
+++ src/graphics/test/TestOrbitBodySpatial.ts
@@ -1,8 +1,11 @@
 import { OrbitBodySpatial } from '../OrbitBodySpatial';
-import { LocalProperty } from '../../core'
+import { LocalProperty, assertIsType } from '../../core'
 import * as THREE from "three";
 
-const TAU = 2 * Math.PI;
+const orbitTestData = require('./orbit-test-data.json')
+
+// Looks nice, typechecks, doesn't work
+// import * as orbitTestData from './orbit-test-data.json'
 
 const mockParent = {};
 
@@ -55,200 +58,25 @@ test('OrbitBodySpatial has no issue with undefined mass', () => {
   expect(spatial.getMass()).toEqual(0);
 });
 
-/// Distance from center to foci, if semi major is 2 and semi minor is 1
-const focusOffset = Math.sqrt(2 * 2 - 1 * 1);
+test('OrbitBodySpatial test data available', () => {
+  expect(orbitTestData.length).toBeGreaterThan(0);
+});
 
-// For these tests base time is 0 and period time is 1
-const orbitPositionTests = [
-  {
-    semiMajor: 1,
-    semiMinor: 1,
-    inclination: 0,
-    ascendingNode: 0,
-    periapsis: 0,
-    expectedPos: [1, 0, 0],
-    mockTime: 0,
-  }, {
-    semiMajor: 1,
-    semiMinor: 1,
-    inclination: 0,
-    ascendingNode: 0,
-    periapsis: 0,
-    expectedPos: [0, 1, 0],
-    mockTime: 0.25,
-  }, {
-    semiMajor: 1,
-    semiMinor: 1,
-    inclination: 0,
-    ascendingNode: 0,
-    periapsis: 0,
-    expectedPos: [-1, 0, 0],
-    mockTime: 0.5,
-  }, {
-    semiMajor: 1,
-    semiMinor: 1,
-    inclination: 0,
-    ascendingNode: 0,
-    periapsis: 0,
-    expectedPos: [1, 0, 0],
-    mockTime: 1,
-  }, {
-    semiMajor: 1,
-    semiMinor: 1,
-    inclination: 0.1 * TAU,
-    ascendingNode: 0,
-    periapsis: 0,
-    expectedPos: [1, 0, 0],
-    mockTime: 0,
-  }, {
-    semiMajor: 1,
-    semiMinor: 1,
-    inclination: 0.1 * TAU,
-    ascendingNode: 0,
-    periapsis: 0,
-    expectedPos: [-1, 0, 0],
-    mockTime: 0.5,
-  }, {
-    semiMajor: 1,
-    semiMinor: 1,
-    inclination: 0.25 * TAU,
-    ascendingNode: 0.25 * TAU,
-    periapsis: 0,
-    expectedPos: [0, 1, 0],
-    mockTime: 0,
-  }, {
-    semiMajor: 1,
-    semiMinor: 1,
-    inclination: 0.25 * TAU,
-    ascendingNode: 0,
-    periapsis: 0.25 * TAU,
-    expectedPos: [0, 0, 1],
-    mockTime: 0,
-  }, {
-    semiMajor: 1,
-    semiMinor: 1,
-    inclination: 0.25 * TAU,
-    ascendingNode: 0.1 * TAU,
-    periapsis: 0.25 * TAU,
-    expectedPos: [0, 0, 1],
-    mockTime: 0,
-  }, {
-    semiMajor: 1,
-    semiMinor: 1,
-    inclination: 0.25 * TAU,
-    ascendingNode: 0,
-    periapsis: 0,
-    expectedPos: [0, 0, 1],
-    mockTime: 0.25,
-  }, {
-    semiMajor: 1,
-    semiMinor: 1,
-    inclination: 0.25 * TAU,
-    ascendingNode: 0.1 * TAU,
-    periapsis: 0,
-    expectedPos: [0, 0, 1],
-    mockTime: 0.25,
-  }, {
-    semiMajor: 1,
-    semiMinor: 1,
-    inclination: 0,
-    ascendingNode: 0.3 * TAU,
-    periapsis: 0.2 * TAU,
-    expectedPos: [-1, 0, 0],
-    mockTime: 0,
-  }, {
-    semiMajor: 2,
-    semiMinor: 1,
-    inclination: 0,
-    ascendingNode: 0,
-    periapsis: 0,
-    expectedPos: [2 - focusOffset, 0, 0],
-    mockTime: 0,
-  }, {
-    semiMajor: 2,
-    semiMinor: 1,
-    inclination: 0,
-    ascendingNode: 0,
-    periapsis: 0,
-    expectedPos: [-2 - focusOffset, 0, 0],
-    mockTime: 0.5,
-  }, {
-    semiMajor: 2,
-    semiMinor: 1,
-    inclination: 0,
-    ascendingNode: 0,
-    periapsis: 0.25 * TAU,
-    expectedPos: [0, 2 - focusOffset, 0],
-    mockTime: 0,
-  }, {
-    semiMajor: 2,
-    semiMinor: 1,
-    inclination: 0,
-    ascendingNode: 0.25 * TAU,
-    periapsis: 0.25 * TAU,
-    expectedPos: [-2 + focusOffset, 0, 0],
-    mockTime: 0,
-  }, {
-    semiMajor: 2,
-    semiMinor: 1,
-    inclination: 0.1 * TAU,
-    ascendingNode: 0.25 * TAU,
-    periapsis: 0.5 * TAU,
-    expectedPos: [0, -2 + focusOffset, 0],
-    mockTime: 0,
-  }, {
-    semiMajor: 2,
-    semiMinor: 1,
-    inclination: 0.1 * TAU,
-    ascendingNode: 0.25 * TAU,
-    periapsis: 0.5 * TAU,
-    expectedPos: [0, -2 + focusOffset, 0],
-    mockTime: 0,
-  }, {
-    semiMajor: 2,
-    semiMinor: 1,
-    inclination: 0.25 * TAU,
-    ascendingNode: 0.25 * TAU,
-    periapsis: 0.25 * TAU,
-    expectedPos: [0, 0, 2 - focusOffset],
-    mockTime: 0,
-  }, {
-    semiMajor: 2,
-    semiMinor: 1,
-    inclination: 0.25 * TAU,
-    ascendingNode: 0.25 * TAU,
-    periapsis: 0.25 * TAU,
-    expectedPos: [0, 0, -2 - focusOffset],
-    mockTime: 0.5,
-  },
-]
-
-// TODO: test parent's position effects position
-// TODO: test non-90 degree angles
-// TODO: test that body is at the correct place at correct times on elliptical orbits
-// TODO: test that base time and period time have desired effect
-// TODO: test velocity
-
-for (const params of orbitPositionTests) {
-  test('OrbitBodySpatial position of ' + JSON.stringify(params), () => {
+for (let i = 0; i < orbitTestData.length; i++) {
+  const testData = orbitTestData[i];
+  test('OrbitBodySpatial ' + JSON.stringify(testData), () => {
     const obj = mockObj();
-    const spatial = new OrbitBodySpatial(mockBodyManager(params.mockTime), obj);
-    obj.orbit.set([
-      params.semiMajor,
-      params.semiMinor,
-      params.inclination,
-      params.ascendingNode,
-      params.periapsis,
-      0, // baseTime
-      1, // periodTime
-      mockParent,
-    ]);
+    const spatial = new OrbitBodySpatial(mockBodyManager(testData.at_time), obj);
+    // Cut off one element at the end (the parent ID, always 1) and replace it with the mock parent object
+    const params = (testData.paramaters.slice(0, -1) as any[]).concat([mockParent])
+    assertIsType(params, [Number, Number, Number, Number, Number, Number, Number, Object]);
+    obj.orbit.set(params);
     const result = new THREE.Vector3();
     spatial.copyPositionInto(result);
     expect(result).toBeCloseToVec(new THREE.Vector3(
-      params.expectedPos[0],
-      params.expectedPos[1],
-      params.expectedPos[2],
+      testData.position[0],
+      testData.position[1],
+      testData.position[2],
     ));
   });
 }
