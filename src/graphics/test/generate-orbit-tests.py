17d7e95e229ea2836bd577b79f2f2b5cdf8e1d6f
Sophie Winter <git@phie.me>
2021-09-12 21:37:35 -0700

    Refactor and add orbit tests

--- src/graphics/test/generate-orbit-tests.py
+++ src/graphics/test/generate-orbit-tests.py
@@ -14,42 +14,42 @@ ambiguous cases.
 '''
 
 import json
-from math import tau, sin, cos
+from math import tau, sin, cos, sqrt
 import os
 
 # Variables used as dictionary keys (slightly prettier code)
 name = 'name'
+grav_param = 'grav_param'
 semi_major = 'semi_major'
 semi_minor = 'semi_minor'
 inclination = 'inclination'
 ascending_node = 'ascending_node'
 periapsis = 'periapsis'
-start_time = 'start_time'
 period_time = 'period_time'
 at_time = 'at_time'
+completed = 'completed'
 position = 'position'
+direction = 'direction'
 velocity = 'velocity'
-unambiguous = 'unambiguous'
 
 # Default values if none specified
 default = {
     name: None,
+    ## Standard gravitational paramater (mass of larger body * gravitational constant)
+    grav_param: 1,
     semi_major: 1,
     semi_minor: 1,
     inclination: 0,
     ascending_node: 0,
     periapsis: 0,
-    start_time: 0,
-    period_time: 1,
-    at_time: 0,
+    completed: 0,
     position: [1, 0, 0],
-    velocity: [0, tau, 0],
+    direction: [0, 1, 0],
 }
 
-# TODO: test non-90 degree angles
 # TODO: test that body is at the correct place at correct times on elliptical orbits
-# TODO: test that base time and period time have desired effect
-# TODO: test velocity
+
+# https://orbitalmechanics.info/ is useful
 
 # Test data
 tests = [
@@ -57,35 +57,124 @@ tests = [
         name: 'start of default orbit',
     }, {
         name: 'quarter past default orbit',
-        at_time: 0.25,
+        completed: 0.25,
         position: [0, 1, 0],
-        velocity: [-tau, 0, 0],
+        direction: [-1, 0, 0],
     }, {
         name: 'half past default orbit',
-        at_time: 0.5,
+        completed: 0.5,
         position: [-1, 0, 0],
-        velocity: [0, -tau, 0],
+        direction: [0, -1, 0],
     }, {
         name: 'full rotation default orbit',
-        at_time: 1,
+        completed: 1,
     }, {
         name: 'start of inclined orbit',
         inclination: 0.1 * tau,
         # position is default
-        velocity: [0, tau * cos(0.1 * tau), tau * sin(0.1 * tau)],
+        direction: [0, cos(0.1 * tau), sin(0.1 * tau)],
     }, {
         name: 'quarter past inclined orbit',
         inclination: 0.1 * tau,
-        at_time: 0.25,
+        completed: 0.25,
         position: [0, cos(0.1 * tau), sin(0.1 * tau)],
-        velocity: [-tau, 0, 0],
+        direction: [-1, 0, 0],
     }, {
         name: 'half past inclined orbit',
         inclination: 0.1 * tau,
-        at_time: 0.5,
+        completed: 0.5,
         position: [-1, 0, 0],
-        velocity: [0, -tau * cos(0.1 * tau), -tau * sin(0.1 * tau)],
-    }]
+        direction: [0, -cos(0.1 * tau), -sin(0.1 * tau)],
+    }, {
+        name: 'start of 90deg ascending node',
+        inclination: 0.1 * tau,
+        ascending_node: 0.25 * tau,
+        position: [0, 1, 0],
+        direction: [-cos(0.1 * tau), 0, sin(0.1 * tau)],
+    }, {
+        name: 'quarter past 90deg ascending node',
+        inclination: 0.1 * tau,
+        ascending_node: 0.25 * tau,
+        completed: 0.25,
+        position: [-cos(0.1 * tau), 0, sin(0.1 * tau)],
+        direction: [0, -1, 0],
+    }, {
+        name: 'half past 90deg ascending node',
+        inclination: 0.1 * tau,
+        ascending_node: 0.25 * tau,
+        completed: 0.5,
+        position: [0, -1, 0],
+        direction: [cos(0.1 * tau), 0, -sin(0.1 * tau)],
+    },
+    # Distance from center to foci is sqrt(a**2 - b**2), so if the semi-major is the largest member of
+    # Pythagorean triple and the semi-minor is either other one, the center-to-focus distance is the
+    # final one. The tests generally use 5 and 3, with the center-to-focus = 4.
+    {
+        name: 'start of flat eliptical',
+        semi_major: 5,
+        semi_minor: 3,
+        position: [1, 0, 0],
+        direction: [0, 1, 0],
+    }, {
+        name: 'half past flat eliptical',
+        semi_major: 5,
+        semi_minor: 3,
+        completed: 0.5,
+        position: [-9, 0, 0],
+        direction: [0, -1, 0],
+    }, {
+        name: 'start of inclined eliptical',
+        semi_major: 5,
+        semi_minor: 3,
+        inclination: 0.1 * tau,
+        position: [1, 0, 0],
+        direction: [0, cos(0.1 * tau), sin(0.1 * tau)],
+    }, {
+        name: 'start of eliptical with 90deg ascending node',
+        semi_major: 5,
+        semi_minor: 3,
+        inclination: 0.1 * tau,
+        ascending_node: 0.25 * tau,
+        position: [0, 1, 0],
+        direction: [-cos(0.1 * tau), 0, sin(0.1 * tau)],
+    }, {
+        name: 'start of eliptical w/ 90deg periapsis',
+        semi_major: 5,
+        semi_minor: 3,
+        inclination: 0.1 * tau,
+        periapsis: 0.25 * tau,
+        position: [0, cos(0.1 * tau), sin(0.1 * tau)],
+        direction: [-1, 0, 0],
+    }, {
+        name: 'half past eliptical with 90deg periapsis',
+        semi_major: 5,
+        semi_minor: 3,
+        inclination: 0.1 * tau,
+        periapsis: 0.25 * tau,
+        completed: 0.5,
+        position: [0, -9 * cos(0.1 * tau), -9 * sin(0.1 * tau)],
+        direction: [1, 0, 0],
+    }, {
+        name: 'start of eliptical with 90deg periapsis and 90deg ascending node',
+        semi_major: 5,
+        semi_minor: 3,
+        inclination: 0.1 * tau,
+        ascending_node: 0.25 * tau,
+        periapsis: 0.25 * tau,
+        position: [-cos(0.1 * tau), 0, sin(0.1 * tau)],
+        direction: [0, -1, 0],
+    }, {
+        name: 'half past eliptical with 90deg periapsis and 90deg ascending node',
+        semi_major: 5,
+        semi_minor: 3,
+        inclination: 0.1 * tau,
+        ascending_node: 0.25 * tau,
+        periapsis: 0.25 * tau,
+        completed: 0.5,
+        position: [9 * cos(0.1 * tau), 0, -9 * sin(0.1 * tau)],
+        direction: [0, 1, 0],
+    },
+    ]
 ''''
     , {
         name: '90deg inclination and 90deg ascending node',
@@ -217,12 +306,25 @@ for i, test in enumerate(tests):
         else:
             names[normalized] = i
 
-if error:
-    exit(1)
+def magnitude(vec):
+    return sqrt(vec[0]**2 + vec[1]**2 + vec[2]**2)
+
+def normalize_to(vec, length):
+    m = magnitude(vec)
+    return [vec[0] * length / m, vec[1] * length / m, vec[2] * length / m]
+
+def get_velocity(test):
+    r = magnitude(test[position])
+    # vis-viva equation
+    speed = sqrt(test[grav_param] * (2 / r - 1 / test[semi_major]))
+    return normalize_to(test[direction], speed)
 
 # Print tests in JSON format
 output = []
 for test in tests:
+    # Kepler's Third Law
+    test_period_time = tau * sqrt(test[semi_major]**3 / test[grav_param])
+    test_at_time = test[completed] * test_period_time
     output.append({
         name: test[name],
         'paramaters': [
@@ -231,14 +333,18 @@ for test in tests:
             test[inclination],
             test[ascending_node],
             test[periapsis],
-            test[start_time],
-            test[period_time],
+            0, # start time
+            test_period_time,
             1, # parent
         ],
-        at_time: test[at_time],
+        grav_param: test[grav_param],
+        at_time: test_at_time,
         position: test[position],
-        velocity: test[velocity],
+        velocity: get_velocity(test),
     })
+
+if error:
+    exit(1)
 
 # Write output files
 print('writing ' + str(len(output)) + ' tests to ' + output_path)
