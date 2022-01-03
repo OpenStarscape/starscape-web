e0ba58981d752771606d75f371398e52ffc687fb
Sophie Winter <git@phie.me>
2022-01-03 15:54:14 -0500

    Game: make actual class called Game instead of GameImpl

--- src/game/Game.ts
+++ src/game/Game.ts
@@ -2,19 +2,7 @@ import { LocalProperty, Lifetime } from '../core';
 import { FramerateReporter } from './FramerateTracker';
 import { SsConnection, SsSessionType, SsObject, SsSet } from '../protocol';
 
-export interface Game {
-  readonly connection: SsConnection;
-  readonly root: SsObject;
-  readonly bodies: SsSet<SsObject>;
-  readonly currentShip: LocalProperty<SsObject | null>;
-  readonly averageFps: FramerateReporter;
-  readonly minFps: FramerateReporter;
-  /// Time in in-game seconds of the current frame. The last starscape timestamp, plus the time
-  /// since it arived and then only updated each rendered frame
-  frameTime(): number;
-}
-
-export class GameImpl extends Lifetime {
+export class Game extends Lifetime {
   readonly connection: SsConnection;
   readonly root: SsObject;
   readonly bodies: SsSet<SsObject>;
