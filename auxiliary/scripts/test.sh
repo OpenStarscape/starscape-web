bc4fa6e1c62533e1c144789f0341b03103f7ad6b
Sophie Winter <git@phie.me>
2022-02-21 21:08:49 -0500

    Remove set o option from scripts for dash compat

--- auxiliary/scripts/test.sh
+++ auxiliary/scripts/test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -eou pipefail
+set -eu pipefail
 if ! test -f package.json; then echo "Script run from $PWD, not project root"; exit 1; fi
 
 npx jest --config ./src/jest/jest.config.json
