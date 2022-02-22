0362eca69fba5ea1d75f85836e5c0a46ae063d5e
William Wold <wm@wmww.sh>
2022-02-21 23:02:02 -0500

    launch.sh: use colors because I can't help myself

--- auxiliary/scripts/launch.sh
+++ auxiliary/scripts/launch.sh
@@ -4,23 +4,33 @@ if ! test -f package.json; then echo "Script run from $PWD, not project root"; e
 
 SERVER_PATH="auxiliary/starscape-server"
 
+COLOR_RED='\x1b[31;1m'
+COLOR_GREEN='\x1b[32;1m'
+COLOR_GRAY='\x1b[37;2m'
+COLOR_NONE='\x1b[0m'
+
 # If there is no server available, give instructions for setting it up
 if ! test -e "$SERVER_PATH"; then
+  printf "$COLOR_RED%s$COLOR_NONE" "error "
   if test -L "$SERVER_PATH"; then
-    echo "ERROR: $SERVER_PATH is a broken symlink to $(readlink "$SERVER_PATH")"
+    echo "is a broken symlink to $(readlink "$SERVER_PATH")"
   else
-    echo "ERROR: $SERVER_PATH does not exist"
+    echo "$SERVER_PATH does not exist"
   fi
   echo
-  echo "If you have not already, clone and build the server:"
+  printf "%s$COLOR_GREEN%s$COLOR_NONE%s\n" "If you have not already, " "clone and build the server" ":"
+  printf "$COLOR_GRAY"
   echo "  cd .."
   echo "  git clone git@github.com:OpenStarscape/starscape-server.git"
   echo "  cd starscape-server"
   echo "  cargo build"
   echo "  cd ../$(basename "$PWD")"
+  printf "$COLOR_NONE"
   echo
-  echo "Once you have the server built, symlink the binary to $SERVER_PATH. for example:"
+  printf "%s$COLOR_GREEN%s$COLOR_NONE%s\n" "Once you have the server built, " "symlink the binary to $SERVER_PATH" ". for example:"
+  printf "$COLOR_GRAY"
   echo "  ln -sf $(dirname "$PWD")/starscape-server/target/debug/starscape-server $SERVER_PATH"
+  printf "$COLOR_NONE"
   exit 1
 fi
 
