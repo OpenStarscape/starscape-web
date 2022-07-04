bbfa85e033e955f00a47b17473aa9d7b4406fe7f
Sophie Winter <git@phie.me>
2022-07-04 14:36:11 -0700

    Allow searching body list

--- src/core/test/TestFilterSetConduit.ts
+++ src/core/test/TestFilterSetConduit.ts
@@ -8,34 +8,48 @@ test('FilterSetConduit gets initial items', () => {
   const localSet = new LocalSetProperty();
   localSet.add(5);
   localSet.add(55);
-  const lt = new DependentLifetime();
-  const filterSet = new FilterSetConduit(lt, localSet, (_lt, item: any) => {
+  const filterSet = new FilterSetConduit(localSet, (_lt, item: any) => {
     return new LocalProperty(item < 10);
   });
-  localSet.add(7);
-  localSet.add(77);
-  expect(Array.from(filterSet.keys())).toEqual([5, 7]);
-  expect(filterSet.has(5)).toBe(true);
-  expect(filterSet.has(55)).toBe(false);
+  const lt = new DependentLifetime();
   filterSet.subscribe(lt, ([lt, item]) => {
     events.push([item, true]);
     lt.addCallback(() => {
       events.push([item, false]);
     });
   });
+  localSet.add(7);
+  localSet.add(77);
+  expect(Array.from(filterSet.keys())).toEqual([5, 7]);
+  expect(filterSet.has(5)).toBe(true);
+  expect(filterSet.has(55)).toBe(false);
   expect(events).toEqual([
     [5, true],
     [7, true],
   ]);
 });
 
+test('FilterSetConduit throws when not yet subscribed to', () => {
+  const localSet = new LocalSetProperty();
+  localSet.add(5);
+  const filterSet = new FilterSetConduit(localSet, (_lt, item: any) => {
+    return new LocalProperty(item < 10);
+  });
+  expect(() => {
+    filterSet.keys();
+  }).toThrow(Error);
+  expect(() => {
+    filterSet.has(5);
+  }).toThrow(Error);
+});
+
 test('FilterSetConduit gets items added to inner set', () => {
   const events: any[] = [];
   const localSet = new LocalSetProperty();
-  const lt = new DependentLifetime();
-  const filterSet = new FilterSetConduit(lt, localSet, (_lt, item: any) => {
+  const filterSet = new FilterSetConduit(localSet, (_lt, item: any) => {
     return new LocalProperty(item < 10);
   });
+  const lt = new DependentLifetime();
   filterSet.subscribe(lt, ([lt, item]) => {
     events.push([item, true]);
     lt.addCallback(() => {
@@ -59,11 +73,11 @@ test('FilterSetConduit item removed and re-added depending on property', () => {
   const events: any[] = [];
   const localSet = new LocalSetProperty();
   const properties = new Map();
-  const lt = new DependentLifetime();
-  const filterSet = new FilterSetConduit(lt, localSet, (_lt, item: any) => {
+  const filterSet = new FilterSetConduit(localSet, (_lt, item: any) => {
     properties.set(item, new LocalProperty(true));
     return properties.get(item);
   });
+  const lt = new DependentLifetime();
   filterSet.subscribe(lt, ([lt, item]) => {
     events.push([item, true]);
     lt.addCallback(() => {
