be92755149f3af35fc2232d1412764ddbe066882
Sophie Winter <git@phie.me>
2022-07-04 13:02:49 -0700

    Add .subscribeWithValueLifetime() tests

--- src/core/test/TestLocalAction.ts
+++ src/core/test/TestLocalAction.ts
@@ -37,3 +37,61 @@ test('LocalAction no subscribers', () => {
   action.fire(7);
   action.fire(false);
 });
+
+test('LocalAction value lifetime', () => {
+  const events: any[] = [];
+  const action = new LocalAction();
+  const lt = new DependentLifetime();
+  action.subscribeWithValueLifetime(lt, (valueLt, value) => {
+    events.push([value, true]);
+    valueLt.addCallback(() => {
+      events.push([value, false]);
+    });
+  });
+  action.fire(3);
+  action.fire(5);
+  expect(events).toEqual([
+    [3, true],
+    [3, false],
+    [5, true],
+  ]);
+});
+
+test('LocalAction value lifetime killed with subscriber lifetime', () => {
+  const events_a: any[] = [];
+  const events_b: any[] = [];
+  const action = new LocalAction();
+  const lt_a = new DependentLifetime();
+  const lt_b = new DependentLifetime();
+  action.subscribeWithValueLifetime(lt_a, (valueLt, value) => {
+    events_a.push([value, true]);
+    valueLt.addCallback(() => {
+      events_a.push([value, false]);
+    });
+  });
+  action.subscribeWithValueLifetime(lt_b, (valueLt, value) => {
+    events_b.push([value, true]);
+    valueLt.addCallback(() => {
+      events_b.push([value, false]);
+    });
+  });
+  action.fire(3);
+  lt_b.kill();
+  expect(events_a).toEqual([
+    [3, true],
+  ]);
+  expect(events_b).toEqual([
+    [3, true],
+    [3, false],
+  ]);
+  action.fire(5);
+  expect(events_a).toEqual([
+    [3, true],
+    [3, false],
+    [5, true],
+  ]);
+  expect(events_b).toEqual([
+    [3, true],
+    [3, false],
+  ]);
+});
