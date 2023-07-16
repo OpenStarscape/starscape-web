8c698420479c2c2189b6d4e61b3bd64d519efa27
Sophie Winter <git@phie.me>
2023-07-16 18:04:56 -0400

    Refactor BodyVisuel

--- src/graphics/BodyVisual.ts
+++ src/graphics/BodyVisual.ts
@@ -1,63 +1,139 @@
 import * as THREE from 'three';
 import { CSS2DObject } from 'three/examples/jsm/renderers/CSS2DRenderer.js';
-import { Lifetime, Vec3, LocalProperty } from '../core';
+import { Lifetime, Vec3, Conduit } from '../core';
 import { SsObject } from "../protocol";
 import { bodyHUD } from "../ui";
 import { ellipticalOrbit } from './ellipticalOrbit'
-import { scalingMesh } from './scalingMesh'
-import type { Body, Game } from '../game'
+import type { Game, Spatial } from '../game'
 import type { Scene } from './Scene'
 
 const yVec = new THREE.Vector3(0, 1, 0);
+const tmpVecA = new THREE.Vector3();
 
-function newCelestialVisual(scene: Scene, lt: Lifetime, body: Body) {
-  const geomProp = new LocalProperty<[geom: THREE.BufferGeometry | null, size: number]>([null, 1]);
-  body.size.getThen(lt, km => {
-    geomProp.set([lt.own(new THREE.SphereBufferGeometry(km, 16, 16)), km]);
+function createColorMaterialPair(
+  lt: Lifetime,
+  color: Conduit<string>
+): [THREE.Material, THREE.Material] {
+  const solidMat = lt.own(new THREE.MeshBasicMaterial({color: 'white'}));
+  const wireMat = lt.own(new THREE.MeshBasicMaterial({color: 'white', wireframe: true}));
+  color.subscribe(lt, color => {
+    solidMat.color.setStyle(color);
+    wireMat.color.setStyle(color);
   });
-
-  const spatial = body.spatial(lt);
-  const hudElement = new CSS2DObject(bodyHUD(lt, spatial));
-  ellipticalOrbit(lt, scene, spatial);
-  scalingMesh(lt, scene, spatial, geomProp, null, [hudElement]);
+  return [solidMat, wireMat];
 }
 
-function newShipVisual(scene: Scene, lt: Lifetime, body: Body) {
-  const direction = new THREE.Vector3();
-  let accel: Vec3 | undefined;
-  body.obj.property('accel', Vec3).subscribe(lt, newAccel => {
-    accel = newAccel;
+function createMesh(lt: Lifetime, scene: Scene): THREE.Mesh {
+  const mesh = new THREE.Mesh();
+  mesh.matrixAutoUpdate = false;
+  scene.addObject(lt, mesh);
+  return mesh;
+}
+
+function createCelestialGeom(lt: Lifetime, mesh: THREE.Mesh, size: number) {
+  mesh.geometry = lt.own(new THREE.SphereBufferGeometry(size, 16, 16));
+}
+
+function createShipGeom(lt: Lifetime, mesh: THREE.Mesh, size: number) {
+  mesh.geometry = lt.own(new THREE.ConeBufferGeometry(1 * size, 3 * size, 16));
+}
+
+function object3DAddChild(lt: Lifetime, parent: THREE.Object3D, child: THREE.Object3D) {
+  parent.add(child);
+  // The mesh is removed from the scene when the lifetime dies, unclear why that doesn't remove
+  // children but it doesn't.
+  lt.addCallback(() => {
+    parent.remove(child);
   });
-  const spatial = body.spatial(lt);
-  const updateQuat = (q: THREE.Quaternion) => {
-    if (accel !== undefined) {
-      accel.copyInto(direction);
-    }
-    if (direction.lengthSq() < 0.0005 && spatial.isReady()) {
-      spatial.copyVelocityInto(direction);
-    }
-    direction.normalize();
-    q.setFromUnitVectors(yVec, direction);
-  };
+}
 
-  const geomProp = new LocalProperty<[geom: THREE.BufferGeometry | null, size: number]>([
-    lt.own(new THREE.ConeBufferGeometry(0.01, 0.03, 16)),
-    0.01
-  ]);
+function scaleMeshToView(
+  scene: Scene,
+  mesh: THREE.Mesh,
+  size: number,
+  matPair: [THREE.Material, THREE.Material],
+) {
+  const dist = mesh.position.distanceTo(scene.camera.position);
+  const scale = dist / 100 / size;
+  if (scale > 1) {
+    mesh.scale.setScalar(scale);
+    mesh.material = matPair[0];
+  } else {
+    mesh.scale.setScalar(1);
+    mesh.material = matPair[1];
+  }
+}
 
-  const hudElement = new CSS2DObject(bodyHUD(lt, spatial));
-  ellipticalOrbit(lt, scene, spatial);
-  scalingMesh(lt, scene, spatial, geomProp, updateQuat, [hudElement]);
+function pointObject3DInDirection(object: THREE.Object3D, direction: THREE.Vector3) {
+  direction.normalize();
+  object.quaternion.setFromUnitVectors(yVec, direction);
+}
+
+function createThrustIndicator(lt: Lifetime, scene: Scene, spatial: Spatial) {
+  const mat = lt.own(new THREE.MeshBasicMaterial({color: 0x20ff40, wireframe: true}));
+  const geom = lt.own(new THREE.ConeGeometry(0.001, 0.01, 3));
+  geom.translate(0, 0.01, 0);
+  const mesh = createMesh(lt, scene);
+  mesh.geometry = geom;
+  const getAccel = spatial.body.obj.property('accel', Vec3).getter(lt);
+  const thrust = new THREE.Vector3();
+  scene.subscribe(lt, () => {
+    thrust.set(0, 0, 0);
+    const accel = getAccel();
+    if (accel) { accel.copyInto(thrust); }
+    const magnitude = thrust.length();
+    if (thrust.lengthSq() > 0.0005 && spatial.isReady()) {
+      spatial.copyPositionInto(mesh.position);
+      scaleMeshToView(scene, mesh, 0.001, [mat, mat]);
+      mesh.scale.multiplyScalar(magnitude);
+      pointObject3DInDirection(mesh, thrust);
+      mesh.updateMatrix();
+      mesh.visible = true;
+    } else {
+      mesh.visible = false;
+    }
+  });
 }
 
 export function newBodyVisual(scene: Scene, game: Game, obj: SsObject) {
   const lt = scene.lt.addDependent(obj.newDependent());
   const body = game.getBody(obj);
+  const spatial = body.spatial(lt);
+  let getSize = body.size.getter(lt);
+
+  const mesh = createMesh(lt, scene);
+  const matPair = createColorMaterialPair(lt, body.color);
+  const hudElement = new CSS2DObject(bodyHUD(lt, spatial));
+  object3DAddChild(lt, mesh, hudElement);
+  ellipticalOrbit(lt, scene, spatial);
+
+  let getAccel: (() => Vec3 | undefined) | null = null;
+  const thrust = new THREE.Vector3();
+  scene.subscribe(lt, () => {
+    spatial.copyPositionInto(mesh.position);
+    const displaySize = (getSize() || 0) * 0.75;
+    scaleMeshToView(scene, mesh, displaySize, matPair);
+    if (getAccel) {
+      thrust.set(0, 0, 0);
+      const accel = getAccel();
+      if (accel) { accel.copyInto(thrust); }
+      if (thrust.lengthSq() < 0.0005) { spatial.copyVelocityInto(thrust); }
+      pointObject3DInDirection(mesh, thrust)
+    }
+    mesh.updateMatrix();
+  });
+
   obj.property('class', String).getThen(lt, cls => {
     if (cls === 'celestial') {
-      newCelestialVisual(scene, lt, body);
+      body.size.getThen(lt, s => {
+        createCelestialGeom(lt, mesh, s);
+      });
     } else if (cls === 'ship') {
-      newShipVisual(scene, lt, body);
+      getAccel = spatial.body.obj.property('accel', Vec3).getter(lt);
+      createThrustIndicator(lt, scene, spatial);
+      body.size.getThen(lt, s => {
+        createShipGeom(lt, mesh, s);
+      });
     } else {
       console.error('unknown body class ', cls);
     }
--- src/graphics/SpaceScene.ts
+++ src/graphics/SpaceScene.ts
@@ -1,7 +1,5 @@
-import * as THREE from 'three';
-import { Lifetime, Vec3 } from '../core';
-import { Game, Spatial } from '../game';
-import { SsObject } from '../protocol';
+import { Lifetime } from '../core';
+import { Game } from '../game';
 import { Scene } from './Scene';
 import { Starfield } from './Starfield';
 import { CameraManager } from './CameraManager';
@@ -12,44 +10,12 @@ class SpaceScene extends Scene {
   readonly starfield: Starfield;
   readonly cameraManager;
 
-  // TODO: move this to Body
-  private readonly thrustMesh: THREE.Mesh;
-
   constructor(
     readonly game: Game,
     lt: Lifetime,
     div: HTMLDivElement,
   ) {
     super(lt, div, game.animation);
-
-    // TODO: move this to Body
-    const mat = this.lt.own(new THREE.MeshBasicMaterial({color: 0x20ff40, wireframe: true}));
-    const geom = this.lt.own(new THREE.ConeGeometry(0.5, 3, 3));
-    geom.translate(0, 4, 0);
-    this.thrustMesh = new THREE.Mesh(geom, mat);
-    this.scene.add(this.thrustMesh);
-    let currentShipSpatial: Spatial | null = null;
-    game.currentShip.subscribeWithValueLifetime(lt, (valueLt, ship) => {
-      currentShipSpatial = ship ? ship.spatial(valueLt) : null;
-    });
-    this.subscribe(lt, () => {
-      if (currentShipSpatial !== null) {
-        currentShipSpatial.copyPositionInto(this.thrustMesh.position);
-      }
-    });
-
-    this.game.currentShip.subscribeWithValueLifetime(this.lt, (currentShipLt, ship) => {
-      if (ship) {
-        console.log('Switching to ship ', ship.obj.id);
-        ship.obj.property('accel', Vec3).subscribe(currentShipLt, accel => {
-          const vec = accel.newThreeVector3();
-          vec.normalize();
-          this.thrustMesh.quaternion.setFromUnitVectors(new THREE.Vector3(0, 1, 0), vec);
-          let len = accel.length();
-          this.thrustMesh.scale.set(len, len, len);
-        });
-      }
-    });
 
     this.normalRenderer.setClearColor('black');
 
