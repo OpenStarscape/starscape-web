f851ad1877205c3e26db8ae459fccb9924b61a46
Sophie Winter <git@phie.me>
2023-07-16 03:14:38 -0400

    Support game pausing visually

--- src/game/AnimationTimer.ts
+++ src/game/AnimationTimer.ts
@@ -4,8 +4,11 @@ import { SsObject } from '../protocol';
 /// Handles calling requestAnimationFrame and keeping track of game time
 export class AnimationTimer extends Conduit<null> {
   private animationFrameTime: number | null = null;
-  private gameTimeOffset: number | null = null;
+  private browserTimeAtLastGameTime: number | null = null;
+  private lastGameTime: number | null = null;
+  private gameTimePerBrowserTime: number = 1;
   private readonly timeProp;
+  private readonly physicsTicksPerNetworkTickProp;
 
   constructor(
     root: SsObject,
@@ -16,6 +19,7 @@ export class AnimationTimer extends Conduit<null> {
   ) {
     super();
     this.timeProp = root.property('time', Number);
+    this.physicsTicksPerNetworkTickProp = root.property('physics_ticks_per_network_tick', Number);
   }
 
   /// Time (in seconds) of last animation frame
@@ -24,7 +28,15 @@ export class AnimationTimer extends Conduit<null> {
   }
 
   gameTime(): number | null {
-    return this.gameTimeOffset !== null ? this.browserTime() + this.gameTimeOffset : null;
+    if (this.browserTimeAtLastGameTime === null ||
+        this.lastGameTime === null
+    ) {
+      return null;
+    } else {
+      return (this.browserTime() - this.browserTimeAtLastGameTime) *
+        this.gameTimePerBrowserTime +
+        this.lastGameTime;
+    }
   }
 
   protected handleAnimFrame(ms: number) {
@@ -38,8 +50,24 @@ export class AnimationTimer extends Conduit<null> {
 
   protected initialSubscriberAdded(hasSubscribersLt: Lifetime): void {
     this.requestAnimFrame(ms => this.handleAnimFrame(ms));
+    this.physicsTicksPerNetworkTickProp.subscribe(hasSubscribersLt, ticks => {
+      if (ticks > 0) {
+        this.gameTimePerBrowserTime = 1;
+      } else {
+        this.gameTimePerBrowserTime = 0;
+      }
+    });
     this.timeProp.subscribe(hasSubscribersLt, gameTime => {
-      this.gameTimeOffset = gameTime - this.browserTime();
+      const browserTime = this.browserTime();
+      if (this.lastGameTime !== null &&
+          this.browserTimeAtLastGameTime !== null &&
+          browserTime - this.browserTimeAtLastGameTime > 0.001
+      ) {
+        this.gameTimePerBrowserTime = (gameTime - this.lastGameTime) /
+          (browserTime - this.browserTimeAtLastGameTime);
+      }
+      this.lastGameTime = gameTime;
+      this.browserTimeAtLastGameTime = this.browserTime();
     });
   }
 
