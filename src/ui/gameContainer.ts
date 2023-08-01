e94d404a96608799f073a48297d1fa8b6b186831
Sophie Winter <git@phie.me>
2023-08-01 04:12:18 -0400

    Fix HTML issue with new bar

--- src/ui/gameContainer.ts
+++ src/ui/gameContainer.ts
@@ -1,16 +1,22 @@
 import { Lifetime, Vec3 } from '../core';
-import { SsObject } from '../protocol';
 import { Game } from '../game';
 import { SpaceScene } from '../graphics';
 import { godControls, shipNameField, gameStats, navPanel, weaponsPanel } from '.';
 
 export function overlay(lt: Lifetime, game: Game): HTMLElement {
   const div = document.createElement('div');
+  div.style.display = 'flex';
+  div.style.flexDirection = 'column';
+  div.style.alignItems = 'flex-start';
+  div.style.pointerEvents = 'none';
   div.appendChild(godControls(lt, game));
   div.appendChild(shipNameField(lt, game));
   div.appendChild(gameStats(lt, game));
   div.appendChild(navPanel(lt, game));
   div.appendChild(weaponsPanel(lt, game));
+  for (let i = 0; i < div.children.length; i++) {
+    (div.children[i] as HTMLElement).style.pointerEvents = 'auto';
+  }
   return div;
 }
 
