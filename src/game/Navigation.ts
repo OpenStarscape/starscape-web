935761dfa1112a2ec3de1240336baa3e64668b92
Sophie Winter <git@phie.me>
2023-08-15 10:00:53 +0200

    Implement flyby mode and real dock mode

--- src/game/Navigation.ts
+++ src/game/Navigation.ts
@@ -6,24 +6,30 @@ export enum Scheme {
   None,
   Orbit,
   Dock,
+  FlyBy,
 }
 
 export interface NoneState {
-  scheme: Scheme.None
+  scheme: Scheme.None,
 }
 
 export interface OrbitState {
-  scheme: Scheme.Orbit
+  scheme: Scheme.Orbit,
   target: Body,
   distance: number | null,
 }
 
 export interface DockState {
-  scheme: Scheme.Dock
+  scheme: Scheme.Dock,
   target: Body,
 }
 
-export type State = NoneState | OrbitState | DockState;
+export interface FlyByState {
+  scheme: Scheme.FlyBy,
+  target: Body,
+}
+
+export type State = NoneState | OrbitState | DockState | FlyByState;
 
 export function applyState(ship: Body, state: State) {
   const obj = ship.obj;
@@ -36,9 +42,11 @@ export function applyState(ship: Body, state: State) {
     obj.property('ap_target', SsObject).set(state.target.obj);
     obj.property('ap_distance', {nullable: Number}).set(state.distance);
   } else if (state.scheme === Scheme.Dock) {
-    obj.property('ap_scheme', String).set('orbit');
+    obj.property('ap_scheme', String).set('dock');
     obj.property('ap_target', SsObject).set(state.target.obj);
-    obj.property('ap_distance', {nullable: Number}).set(0);
+  } else if (state.scheme === Scheme.FlyBy) {
+    obj.property('ap_scheme', String).set('flyby');
+    obj.property('ap_target', SsObject).set(state.target.obj);
   }
   else {
     console.error('Invalid autopilot state');
